715e4431800ca7f6811d33733ec73968f3c0ede7
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2025-11-27 15:35:25 +0000

    Version.h added.

---
+++ Version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// [R].[M].[F].[D]
+// [R] -> Release
+// [M] -> Major Chances
+// [F] -> Functional updating
+// [D] -> Debugging
+#define VERSION "0.0.0.0"
+
+#endif //!VERSION
