cc6877ba3c44ffc166bd0d16f4f026b0082bab2d
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2025-11-27 16:07:32 +0000

    ActionItem.h defined.

--- src/Menu/ActionItem.h
+++ src/Menu/ActionItem.h
@@ -1,7 +1,9 @@
 #ifndef ACTIONITEM_H
 #define ACTIONITEM_H
 
+#include <iostream>
 #include <functional>
+#include <limits>
 
 #include "IMenuItem.h"
 
@@ -9,9 +11,22 @@ class ActionItem : public IMenuItem {
     public:
         ActionItem(const std::string& title, std::function<void()> actionFunc) 
         : IMenuItem(title), action(actionFunc) {}
+
+        bool execute() override {
+            try {
+                action();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cin.get();
+            }
+            catch(const std::exception& e) {
+                std::cerr << e.what() << '\n';
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cin.get();
+            }
+            return true;
+        }
     private:
         std::function<void()> action;
-    
 };
 
 
